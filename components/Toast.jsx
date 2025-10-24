ce3a13ffeb2239c0c7bb9ea51a34935da7728510
Chukwu Miracle <mimigolld@gmail.com>
2025-10-24 21:11:00 +0100

    toast logic

--- components/Toast.jsx
+++ components/Toast.jsx
@@ -3,7 +3,16 @@ import { useEffect } from "react";
 export default function Toast({message, type="info", onClose, }){
    useEffect(() => {
     if (!message) return;
-    const timer = setTimeout(() => {
-      onClose();
+    const timer = setTimeout(() => onClose(), duration)
+      return () => clearTimeout(timer);
     }, [message, duration, onClose]);
-})
+
+    if (!message) return null;
+
+    return (
+        <div className={`toast toast-${type}`}>
+            <span>{message}</span>
+            <button onClick={() => onClose?.()}>X</button>
+        </div>
+    )
+}
